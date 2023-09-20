0111062c4d2c3a5e9ad5a7d09931240cf266fed8
abduhamidbotirov <abduhamidbotirovwork@gmail.com>
2023-09-20 17:10:53 +0300

    Initial commit

---
+++ lesson-5.py
@@ -0,0 +1,23 @@
+# a, b =map(int, input().split())
+# for i in range(0, b):
+#     print(a)
+# a = int(input("son kiriting: "))
+# sumr =0;
+# for i in range(0)
+# step = 0.2
+# start = 1
+# end = 2.2
+#
+# for i in range(int(start / step), int(end / step)):
+#     weight = i * step
+#     price = weight * a
+#     print(f"{weight:.1f} kg narxi {price} bo'ladi")
+# sum =0
+# for i in range(0, a):
+    #     print(f"{i} tub son")
+    # else:print(f"{i} tub emas")
+
+
+
+
+    
