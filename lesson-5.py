42c3c6025a29ca22572364a7891ec18282dfe94c
abduhamidbotirov <abduhamidbotirovwork@gmail.com>
2023-09-20 18:58:16 +0300

    nima gap

--- lesson-5.py
+++ lesson-5.py
@@ -17,7 +17,53 @@
     #     print(f"{i} tub son")
     # else:print(f"{i} tub emas")
 
+# a = int(input("son kiriting: "))
+# b = int(input("son kiriting: "))
+#
+# while a>b:
+#     a-=b
+# print(a)
+
+# Butun sonni olish
+# n = int(input("Istalgan butun sonni kiriting: "))
+# a=1
+# s=0
+# while a<n:
+#     a*=2
+#     s+=1
+# print(s)
+
+# while a<n:
+#     a*=3
+# if a==n:
+#     print(True)
+# else:
+#     print(False)
+# 3 ning darajasi bo'lsa
+# is_three_power = False
+# current_power = 1
+# while current_power <= n:
+#     if current_power == n:
+#         is_three_power = True
+#     current_power *= 3
+#
+# if is_three_power:
+#     print(f"{n} 3 - ning darajasi")
+# else:
+#     print(f"{n} 3 - ning darajasi emas")
 
 
 
-    
+def nima_gapni_chiqar(a):
+    print(a)
+nima_gapni_chiqar("nima gap")
+
+
+
+
+
+
+
+
+
+
