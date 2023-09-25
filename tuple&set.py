3db6d4321da435d8944665618b29f2fc20e2cd64
abduhamidbotirov <abduhamidbotirovwork@gmail.com>
2023-09-25 18:18:18 +0300

    oke

--- tuple&set.py
+++ tuple&set.py
@@ -1,2 +1,14 @@
-b=(1,2,3,"log","log")
-print(b.index(2))
+# b=(1,2,3,"log","log")
+# print(b.index(2))
+b={"chilonzor", "bodamzor" }
+c={"chilonzor", "bodamzor2"}
+# b.add("bodamzor")
+# b.discard("bodamzor")
+# b.update(c)
+# b.pop()
+# b.clear()
+# del b
+# d= b.union(c)
+# print(d)
+bob = b.intersection(c)
+print(bob)
