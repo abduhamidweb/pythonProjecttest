2589a2cb7c92ca766099ce988a8b73d9a9211614
abduhamidbotirov <abduhamidbotirovwork@gmail.com>
2023-09-27 15:44:11 +0300

    oke

--- tuple&set.py
+++ tuple&set.py
@@ -1,7 +1,7 @@
 # b=(1,2,3,"log","log")
 # print(b.index(2))
-b={"chilonzor", "bodamzor" }
-c={"chilonzor", "bodamzor2"}
+# b={"chilonzor", "bodamzor" }
+# c={"chilonzor", "bodamzor2"}
 # b.add("bodamzor")
 # b.discard("bodamzor")
 # b.update(c)
@@ -10,5 +10,17 @@ c={"chilonzor", "bodamzor2"}
 # del b
 # d= b.union(c)
 # print(d)
-bob = b.intersection(c)
-print(bob)
+# bob = b.intersection(c)
+# b.symmetric_difference_update(c)
+# new = b.symmetric_difference(c)
+# print(new)
+# a =("a")
+# name = str(input("name kiriting: "))
+# a = list(("apple", "banana", "cherry", "apricot"))
+# print(a[::-1])
+# a.sort()
+
+# for item in a:
+#     if name in item:
+#         a.remove(item)
+# print(tuple(a))
---
+++ uyga8.py
@@ -0,0 +1,65 @@
+# 1
+# my_set = {1, 2, 3, 4, 5}
+# length = len(my_set)
+# print(length)
+# 2
+# my_set = {1, 2, 3}
+# my_set.add(4)
+# print(my_set)
+# 3
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# result_set = set1.union(set2)
+# print(result_set)
+# 4
+# my_set = {1, 2, 3, 4, 5}
+# my_set.remove(3)
+# print(my_set)
+# 5
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# result_set = set1.intersection(set2)
+# print(result_set)
+# 6
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 4}
+# set1.difference_update(set2)
+# print(set1)
+# 7
+# set1 = {5, 7, 2, 1}
+# set2 = {3, 8, 1}
+# min_element = min(set1.union(set2))
+# print(min_element)
+# 8
+# my_set = {1, 2, 3, 4, 5}
+# element = int(input("O'chiriladigan elementni kiriting: "))
+# my_set.discard(element)
+# print(my_set)
+# 9
+# my_tuple = ()
+# element = input("Tuple'ga qo'shiladigan elementni kiriting: ")
+# my_tuple += (element,)
+# print(my_tuple)
+# 10
+# ????
+# 11
+# my_tuple = (1, 2, 3, 4, 5)
+# reversed_tuple = my_tuple[::-1]
+# print(reversed_tuple)
+# 12
+# ???
+# 13
+# ??
+# 14
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 4}
+# set1.intersection_update(set2)
+# print(set1)
+# 15
+# my_set = {1, 2, 3, 4, 5}
+# element = int(input("Tekshiriladigan elementni kiriting: "))
+# if element in my_set:
+#     print(f"{element} setda mavjud.")
+# else:
+#     print(f"{element} setda mavjud emas.")
+# 16
